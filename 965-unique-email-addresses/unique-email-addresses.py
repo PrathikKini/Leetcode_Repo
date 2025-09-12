6d4a89519b3340f015c401854ae80dd436eb2a7f
Prathik Kini M <64038029+PrathikKini@users.noreply.github.com>
2025-09-12 12:51:03 +0530

    Time: 3 ms (86.34%) | Memory: 18 MB (25.70%) - LeetSync

---
+++ 965-unique-email-addresses/unique-email-addresses.py
@@ -0,0 +1,7 @@
+class Solution:
+    def numUniqueEmails(self, emails: List[str]) -> int:
+        def parse(email):
+            local, domain = email.split('@')
+            local = local.split('+')[0].replace('.',"")
+            return f"{local}@{domain}"
+        return len(set(map(parse,emails)))
