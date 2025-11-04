5a1e6dd3026dddb6aa8867b9f97774d9e747284b
Prathik Kini M <64038029+PrathikKini@users.noreply.github.com>
2025-11-04 14:51:39 +0530

    Time: 43 ms (7.01%) | Memory: 17.9 MB (38.70%) - LeetSync

---
+++ 3610-find-x-sum-of-all-k-long-subarrays-i/find-x-sum-of-all-k-long-subarrays-i.py
@@ -0,0 +1,8 @@
+class Solution:
+    def findXSum(self, nums: List[int], k: int, x: int) -> List[int]:
+        def do_sum(idx : int) -> int:
+            ctr = Counter(nums[idx:idx + k])
+            most_freq = nlargest(x, ctr, key = lambda y : (ctr[y], y))
+            return sum(map(lambda y:y * ctr[y], most_freq))
+        
+        return list(map(do_sum, range(len(nums)+1 - k)))
