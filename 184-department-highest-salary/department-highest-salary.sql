a05b97e2e8ba5b15da03524444dca9cdabbe4e14
Prathik Kini M <64038029+PrathikKini@users.noreply.github.com>
2026-03-22 20:04:56 +0530

    Time: 389 ms (84.70%) | Memory: 0B (100.00%) - LeetSync

---
+++ 184-department-highest-salary/department-highest-salary.sql
@@ -0,0 +1,11 @@
+/* Write your T-SQL query statement below */
+with cte as (
+select 
+d.name as Department, e.name as Employee, Salary, rank() over (partition by departmentId order by salary desc) as rk
+from Employee e
+join Department d
+on e.departmentId = d.id
+)
+select Department, Employee, Salary
+from cte where rk = 1
+
