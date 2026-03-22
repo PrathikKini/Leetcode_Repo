7489e0549c380dacb95dd46a7f1a97d15c71cbce
Prathik Kini M <64038029+PrathikKini@users.noreply.github.com>
2026-03-22 15:37:12 +0530

    Time: 284 ms (81.33%) | Memory: 0B (100.00%) - LeetSync

---
+++ 262-trips-and-users/trips-and-users.sql
@@ -0,0 +1,12 @@
+/* Write your T-SQL query statement below */
+select request_at as Day,
+round(sum(case when status != 'completed' then 1.0 else 0 end) / count(status),2) as 'Cancellation Rate'
+from Trips 
+where request_at between '2013-10-01' and '2013-10-03' 
+and 
+client_id not in 
+(select users_id from Users where banned = 'Yes') 
+and 
+driver_id not in 
+(select users_id from Users where banned = 'Yes')
+group by request_at
